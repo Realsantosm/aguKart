08153dbe7ed327e1372a9bd743bf7ec5f1524de8
Santos <realsantosm@gmail.com>
2025-09-13 14:24:29 +0530

    test :
     app style of tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@
 
 function App() {
   return (
-    <div>
-      <h1 className="text-8xl">AguKart 🛍️ | Online Shoppe</h1>
+    <>
+      <div className="flex justify-center h-screen bg-slate-200">
+        <h1 className="text-slate-800 mt-2 text-2xl bold">AguKart 🛍️ | Online Shoppe</h1>
     </div>
+    </>
   )
 }
 
